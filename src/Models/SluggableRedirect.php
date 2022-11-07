5d7bfc8c98ff8d62743e66bea6f4cab3fa4d9936
Viacheslav Radionov <radionov@gmail.com>
2022-11-07 13:36:35 +0100

    Laravel 9.x compatible

--- src/Models/SluggableRedirect.php
+++ src/Models/SluggableRedirect.php
@@ -3,14 +3,13 @@ namespace Vanderb\SluggableRedirect\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SluggableRedirect extends Model {
-
+class SluggableRedirect extends Model
+{
     protected $table = 'sluggable_redirects';
-    
+
     protected $fillable = ['slug', 'sluggable_id', 'sluggable_type'];
-    
+
     public function sluggable(){
-        return $this->morphTo();   
+        return $this->morphTo();
     }
-    
 }
